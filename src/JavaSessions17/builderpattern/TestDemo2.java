3063b416b28694b307182e8efd659785fec4f549
ayeshayusufgit <ayesha.yusuf@gmail.com>
2022-11-04 20:53:20 +0530

    reason

---
+++ src/JavaSessions17/builderpattern/TestDemo2.java
@@ -0,0 +1,10 @@
+package JavaSessions17.builderpattern;
+
+public class TestDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Demo");
+	}
+
+}

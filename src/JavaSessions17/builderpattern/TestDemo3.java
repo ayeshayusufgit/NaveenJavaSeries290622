f8c597b05e31fbfad520776029f1752bad1c1bea
ayeshayusufgit <ayesha.yusuf@gmail.com>
2022-11-04 23:35:01 +0530

    reason

--- src/JavaSessions17/builderpattern/TestDemo3.java
+++ src/JavaSessions17/builderpattern/TestDemo3.java
@@ -1,11 +1,10 @@
 package JavaSessions17.builderpattern;
 
-public class TestDemo2 {
+public class TestDemo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Demo1");
-		System.out.println("Test Demo2");
+		System.out.println("Test Demo3");
 	}
 
 }

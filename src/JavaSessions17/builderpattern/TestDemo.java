8cfc9fc45fb09a8110ef62693f80d70fd38fdaeb
ayeshayusufgit <ayesha.yusuf@gmail.com>
2022-11-08 12:39:58 +0530

    updated TestAmazon.java file and created TestDemo.java

---
+++ src/JavaSessions17/builderpattern/TestDemo.java
@@ -0,0 +1,12 @@
+package JavaSessions17.builderpattern;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Demo");
+
+	}
+
+}
